be6453df9657df405d8d7af236d99e7e0df26513
w.h.sinn <w.h.sinn@gmail.com>
2019-03-22 06:34:27 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
   i=0
   select=[]
   while i < collection.length
-    if (yield(collection[i])
+    if (yield(collection[i]))
       select << collection[i]
-      )
+      
 end
